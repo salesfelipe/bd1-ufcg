5875ad7516cb85ff1d2a8ab87a2459d9db78ca7d
Samantha Monteiro <samanthakem@moon.home>
2017-03-10 14:37:28 -0300

    alterando inconsistencia no tipo do atributo cpf

--- ddl_grupo08.sql
+++ ddl_grupo08.sql
@@ -1,14 +1,3 @@
-CREATE TABLE TB_Cliente
-(
-    Cpf VARCHAR(11) NOT NULL,
-    Nome VARCHAR(100) NOT NULL,
-    Data_Nascimento DATE NOT NULL,
-    Sexo CHAR NOT NULL,
-    Endereco_Id INT NOT NULL,
-    PRIMARY KEY (Cpf),
-    FOREIGN KEY (Endereco_Id) REFERENCES TB_Endereco (Id)
-);
-
 CREATE TABLE TB_Endereco
 (
     Id INT NOT NULL,
@@ -17,6 +6,17 @@ CREATE TABLE TB_Endereco
     Bairro VARCHAR(15) NOT NULL,
     Cep CHAR(8) NOT NULL,
     PRIMARY KEY (Id)
+);
+
+CREATE TABLE TB_Cliente
+(
+    Cpf CHAR(11) NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    Data_Nascimento DATE NOT NULL,
+    Sexo CHAR NOT NULL,
+    Endereco_Id INT NOT NULL,
+    PRIMARY KEY (Cpf),
+    FOREIGN KEY (Endereco_Id) REFERENCES TB_Endereco (Id)
 );
 
 CREATE TABLE TB_Dependente
@@ -30,6 +30,14 @@ CREATE TABLE TB_Dependente
 
 );
 
+CREATE TABLE TB_Funcao
+(
+  Id INT NOT NULL,
+  Nome VARCHAR(25) NOT NULL,
+  PRIMARY KEY (Id),
+  UNIQUE (Nome)
+);
+
 CREATE TABLE TB_Funcionario
 (
     Nome VARCHAR(100) NOT NULL,
@@ -39,12 +47,4 @@ CREATE TABLE TB_Funcionario
     Funcao_Id INT NOT NULL,
     PRIMARY KEY(Cpf),
     FOREIGN KEY (Funcao_Id) REFERENCES TB_Funcao(Id)
-);
-
-CREATE TABLE TB_Funcao
-(
-  Id INT NOT NULL,
-  Nome VARCHAR(25) NOT NULL,
-  PRIMARY KEY (Id),
-  UNIQUE (Nome)
 );

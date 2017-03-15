7f98d2a71f1f83201b1797589dd86dd045bd8cfa
Samantha Monteiro <samanthakem@gmail.com>
2017-03-15 13:40:11 -0300

    Adicionando um row na tabela cliente

---
+++ insert_grupo08.sql
@@ -0,0 +1,2 @@
+INSERT INTO cliente (cpf, nome, data_nascimento, sexo, rua, numero, bairro, cep, email)
+   VALUES ('01123367787', 'Joao Matheus', TO_DATE('30/03/1990', 'dd/mm/yyyy'), 'M', 'Rua do Sol', 30, 'Santa Rosa', '58417333', 'joaomatheus@email.com');

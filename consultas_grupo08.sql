3da0b25d43085a9396cbd7051f0d3f389faed9d7
Samantha Monteiro <samanthakem@gmail.com>
2017-03-15 15:23:15 -0300

    Adicionando nova consulta

--- consultas_grupo08.sql
+++ consultas_grupo08.sql
@@ -1,3 +1,17 @@
+
+/* Consulta referente a questao #8 */
+
+SELECT * FROM cliente c
+JOIN hospedagem h
+ON h.cpf_cliente = c.cpf
+WHERE c.data_nascimento = (
+  SELECT MAX(data_nascimento) FROM (SELECT * FROM cliente
+  JOIN hospedagem
+  ON cpf_cliente = cpf)
+);
+
+/* Consulta referente a questao #8 */
+
 SELECT COUNT(DISTINCT c.cpf) FROM cliente c
 INNER JOIN dependente d
-ON c.cpf = d.titular AND c.sexo='F'
+ON c.cpf = d.titular AND c.sexo='F';
